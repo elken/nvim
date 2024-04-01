d93dc0841f80725c42b0bc52787378db52497113
Ellis Kenyo <me@elken.dev>
2024-04-01 16:02:22 +0100

    fix(keybindings): add some more shortcut binds & docs
    Slightly clarify some structure and add a bunch of useful quick bindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,14 +1,24 @@
+-- Long-time muscle memory
 vim.keymap.set("n", "<C-s>", "<cmd>w<cr>", { silent = true, desc = "Save buffer" })
 
+-- Slightly faster window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
+-- Global leader binds
+vim.keymap.set("n", "<leader>;", "<cmd>nohl<cr>", { desc = "Turn off search highlight" })
+vim.keymap.set("n", "<leader>l", ":lua ", { desc = "Open lua prompt"})
+vim.keymap.set("n", "<leader>!", ":! ", { desc = "Open shell prompt"})
+vim.keymap.set("n", "<leader>%", "<cmd>so %<cr>", { desc = "Reload current file"})
+
+-- [B]uffer
 vim.keymap.set("n", "<leader>bd", "<cmd>Bdelete<cr>", { desc = "Kill buffer" })
 vim.keymap.set("n", "<leader>bp", "<cmd>bp<cr>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>bn", "<cmd>bn<cr>", { desc = "Next buffer" })
 
+-- [F]ile
 vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
 
 local function yank_to_clipboard(value)
@@ -20,8 +30,7 @@ end
 vim.keymap.set("n", "<leader>fy", yank_to_clipboard("%:p"), { desc = "Yank file path" })
 vim.keymap.set("n", "<leader>fY", yank_to_clipboard("%:~:."), { desc = "Yank file path" })
 
-vim.keymap.set("n", "<leader>;", "<cmd>nohl<cr>", { desc = "Turn off search highlight" })
-
+-- [O]pen
 local function open_in_folder()
   local format = "<cmd>!xdg-open %s<cr>"
   if vim.fn.has("mac") == 1 then
@@ -42,13 +51,18 @@ end
 
 vim.keymap.set("n", "<leader>oo", open_in_folder(), { desc = open_in_folder_label(), silent = true })
 
-vim.keymap.set("n", "<leader>hb", "<cmd>Gitsigns blame_line<cr>", { desc = "Blame line" })
-vim.keymap.set("n", "<leader>hn", "<cmd>Gitsigns next_hunk<cr>", { desc = "Next hunk" })
-vim.keymap.set("n", "<leader>hp", "<cmd>Gitsigns blame_line<cr>", { desc = "Previous hunk" })
-vim.keymap.set("n", "<leader>hP", "<cmd>Gitsigns blame_line<cr>", { desc = "Preview hunk" })
-vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset current hunk" })
-vim.keymap.set("n", "<leader>hR", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset current buffer" })
-vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns blame_line<cr>", { desc = "Stage current hunk" })
-vim.keymap.set("n", "<leader>hS", "<cmd>Gitsigns blame_line<cr>", { desc = "Stage current buffer" })
-vim.keymap.set("n", "<leader>hu", "<cmd>Gitsigns blame_line<cr>", { desc = "Undo stage hunk" })
-vim.keymap.set("n", "<leader>hU", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset buffer index" })
+-- [G]it
+vim.keymap.set("n", "<leader>gB", "<cmd>Gitsigns blame_line<cr>", { desc = "Blame line" })
+vim.keymap.set("n", "<leader>gn", "<cmd>Gitsigns next_hunk<cr>", { desc = "Next hunk" })
+vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns blame_line<cr>", { desc = "Previous hunk" })
+vim.keymap.set("n", "<leader>gP", "<cmd>Gitsigns blame_line<cr>", { desc = "Preview hunk" })
+vim.keymap.set("n", "<leader>gr", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset current hunk" })
+vim.keymap.set("n", "<leader>gR", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset current buffer" })
+vim.keymap.set("n", "<leader>gs", "<cmd>Gitsigns blame_line<cr>", { desc = "Stage current hunk" })
+vim.keymap.set("n", "<leader>gS", "<cmd>Gitsigns blame_line<cr>", { desc = "Stage current buffer" })
+vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns blame_line<cr>", { desc = "Undo stage hunk" })
+vim.keymap.set("n", "<leader>gU", "<cmd>Gitsigns blame_line<cr>", { desc = "Reset buffer index" })
+
+-- [H]elp/quick prompts
+vim.keymap.set("n", "<leader>ht", "<cmd>Telescope help_tags<cr>", { desc = "Search help tags"})
+vim.keymap.set("n", "<leader>hh", ":h ", { desc = "Open help prompt"})
