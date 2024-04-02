954daac992d1e8feebe80acb9934bbc13f713b22
Ellis Kenyo <me@elken.dev>
2024-04-02 09:08:45 +0100

    feat: add trouble & better diagnostic signs
    Trying this out for now, might not last.
    
    Another win here here is nicer-looking diagnostic signs

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,43 @@
+--[[
+A nicer UI for diagnostics. Also includes some extra panes for workspace diagnostics and the like.
+--]]
+
+return {
+  {
+    "folke/trouble.nvim",
+    branch = "dev", -- IMPORTANT!
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>xl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+  },
+}
