0e5c0f565a58c37c1ec79bc59d190b04c5587631
Ellis Kenyo <me@elken.dev>
2024-04-07 15:46:42 +0100

    fix(flash): add jump_labels to f,F,t and T motions

--- lua/specs/init.lua
+++ lua/specs/init.lua
@@ -86,7 +86,13 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
