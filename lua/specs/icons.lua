69e4eed7f8689762ddcdb20302c99af945770bbc
Ellis Kenyo <me@elken.dev>
2024-07-21 07:52:13 +0100

    feat: add some more icons
    Customize some of the missing and default icons for rails

---
+++ lua/specs/icons.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  config = function()
+    local devicons = require("nvim-web-devicons")
+    local dotenv_icon, dotenv_color = devicons.get_icon_color(".env", ".env")
+    local ruby_icon, _ = devicons.get_icon_color("test.rb", "rb")
+    local ruby_color = "#a32a24"
+
+    local icons = {
+      rb = { icon = ruby_icon, color = ruby_color, name = "Rb" },
+      rake = { icon = ruby_icon, color = ruby_color, name = "Rake" },
+      rakefile = { icon = ruby_icon, color = ruby_color, name = "RakeFile" },
+      gemfile = { icon = ruby_icon, color = ruby_color, name = "GemFile" },
+      brewfile = { icon = ruby_icon, color = ruby_color, name = "BrewFile" },
+      ["config.ru"] = { icon = ruby_icon, color = ruby_color, name = "config.ru" },
+      [".ruby-version"] = { icon = ruby_icon, color = ruby_color, name = "config.ru" },
+      jbuilder = { icon = ruby_icon, color = "#f7cd6d", name = "Jbuilder" },
+      [".env.development"] = { icon = dotenv_icon, color = dotenv_color, name = ".env" },
+      [".env.development.local"] = { icon = dotenv_icon, color = dotenv_color, name = ".env" },
+      [".env.test"] = { icon = dotenv_icon, color = dotenv_color, name = ".env" },
+      ["Procfile.dev"] = { icon = "󰫏", color = ruby_color, name = ".rspec" },
+      [".rspec"] = { icon = "󰫏", color = ruby_color, name = ".rspec" },
+      [".sonarcloud.properties"] = { icon = "󱇲", color = "#f3702a", name = "sonarcloud" },
+      ["yarn.lock"] = { icon = "", color = "#ffffff", name = "yarn.lock" },
+    }
+
+    devicons.set_icon(icons)
+  end,
+}
