8f3f5f822835d7af2f97cf1dc9580b7f84ef300e
Ellis Kenyo <me@elken.dev>
2024-07-23 08:09:58 +0100

    feat(markdown): add code block evaluation

--- lua/specs/markdown.lua
+++ lua/specs/markdown.lua
@@ -13,4 +13,11 @@ return {
     },
     opts = {},
   },
+  {
+    "gpanders/vim-medieval",
+    config = function()
+      -- TODO: Add some kind of command to generate the results block and the comment
+      vim.g.medieval_langs = { "python=python3", "ruby", "sh", "console=bash" }
+    end,
+  },
 }
