a14f05e429044971bddbf69011469c537b06e020
Ellis Kenyo <me@elken.dev>
2024-08-08 10:08:41 +0100

    feat: add image.nvim

---
+++ lua/specs/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "3rd/image.nvim",
+    opts = {
+      integrations = {
+        markdown = {
+          clear_in_insert_mode = true,
+        },
+      },
+    },
+    dependencies = {
+      "leafo/magick",
+    },
+  },
+}
